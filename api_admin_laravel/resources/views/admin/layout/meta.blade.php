286e1586b9e9913dd828b7d8026dbf74b28e6bba
nguyenduythin <59800849+nguyenduythin@users.noreply.github.com>
2021-11-10 21:35:53 +0700

    change color template

--- api_admin_laravel/resources/views/admin/layout/meta.blade.php
+++ api_admin_laravel/resources/views/admin/layout/meta.blade.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
 <meta name="description"
-  content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+  content="timenail-Mang đến sự lựa chọn tốt nhất !">
 <meta name="keywords" content="admin timenail, nail store">
 <meta name="author" content="TimeNail">
 <meta name="csrf-token" content="{{ csrf_token() }}">
--- api_admin_laravel/resources/views/admin/layout/style.blade.php
+++ api_admin_laravel/resources/views/admin/layout/style.blade.php
@@ -1,6 +1,6 @@
 <link rel="apple-touch-icon" href="{{ asset('admin/images/ico/apple-icon-120.html')}}">
-<link rel="shortcut icon" type="image/x-icon"
-  href="https://pixinvent.com/demo/vuexy-html-bootstrap-admin-template/app-assets/images/ico/favicon.ico">
+<link rel="shortcut icon" type="image/png"
+  href="{{ asset('admin/images/logo/logo.png')}}">
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
   rel="stylesheet">
 
